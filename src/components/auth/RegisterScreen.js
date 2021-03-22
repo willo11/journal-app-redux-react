bc7092596da507b08313f246af020ba6a614d792
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-22 18:07:23 -0500

    register style

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <>
+      <h3 className="auth__title">Register</h3>
+      <form>
+      <input
+          className="auth__input"
+          type="text"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Confirm Password"
+          name="password2"
+        />
+        <button className="btn btn-primary btn-block mb-5" type="submit">
+          Register
+        </button>
+        
+        <Link to="/auth/login" className="link">
+          Already Registered?
+        </Link>
+      </form>
+    </>
     )
 }

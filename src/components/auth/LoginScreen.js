8d035af23703574027765daa46f3d72768117068
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-22 17:52:23 -0500

    login router style

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
-    return (
-        <>
-            <h3>Login</h3>
-            <form>
-                <input 
-                    type="text"
-                    placeholder="email"
-                    name="email"
-                />
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                />
-                <button
-                type="submit">
-                    Login
-                </button>
-                <hr/>
-                Google
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <h3 className="auth__title">Login</h3>
+      <form>
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Login
+        </button>
+        <div className="auth__social-networks">
+          <p> Login With Social Network: </p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to="auth/register">Create New Account</Link>
+      </form>
+    </>
+  );
+};

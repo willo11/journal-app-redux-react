b7cb3bc63eaead3244f36a0d3f0f24c50fd598f7
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-22 20:13:33 -0500

    HomeScreen structure and style

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input 
+                    type="text"
+                    placeholder="Some Awesome Title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happend Today"
+                    className="notes__textarea"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img 
+                        src="https://static.toiimg.com/photo/72975551.cms"
+                        alt="imagen xD"
+                    />
+                </div>
+
+            </div>
         </div>
     )
 }

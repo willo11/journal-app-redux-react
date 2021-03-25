0df6b76fe84976c8180e88b917ece04a1bcdfd03
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-25 11:28:05 -0500

    Some Animations

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -32,7 +32,7 @@ export const LoginScreen = () => {
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleLogin} className="animate__animated animate__fadeIn animate_faster">
         <input
           className="auth__input"
           type="text"

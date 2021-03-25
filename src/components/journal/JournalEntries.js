6abb14431986f6b9a8c6b1d0f76d85e8111ebb67
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-25 10:20:35 -0500

    upload cloudinary image

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -5,7 +5,6 @@ import { JournalEntry } from './JournalEntry';
 export const JournalEntries = () => {
 
     const { notes } = useSelector( state => state.notes );
-    console.log(notes);
 
     return (
         <div className="journal__entries">

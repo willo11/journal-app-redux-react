83568e93d3d3cbe34a76d9f2d2727022b0ac1960
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-25 09:31:08 -0500

    update sidebar and save message

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,53 +1,67 @@
+import Swal from "sweetalert2";
 import { db } from "../firebase/firebase-config";
 import { loadNotes } from "../helpers/loadNotes";
 import { types } from "../types/types";
 
 export const startNewNote = () => {
-  return async(dispatch, getState) => {
-
-    const {uid} = getState().auth;
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
     const newNote = {
-        title: '',
-        body: '',
-        date: new Date().getTime()
-    }
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
 
-     const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
+    const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
 
-     dispatch( activeNote(doc.id , newNote));
+    dispatch(activeNote(doc.id, newNote));
   };
-}
+};
 
-export const activeNote = (id,note) => ({
-    type: types.notesActive,
-    payload: {
-        id,
-        ...note
-    }
-})
+export const activeNote = (id, note) => ({
+  type: types.notesActive,
+  payload: {
+    id,
+    ...note,
+  },
+});
 
-export const startLoadingNotes = (uid) =>{
-    return async(dispatch) => {
-        const notes = await loadNotes(uid);
-        dispatch(setNotes(notes));
-    }
-}
+export const startLoadingNotes = (uid) => {
+  return async (dispatch) => {
+    const notes = await loadNotes(uid);
+    dispatch(setNotes(notes));
+  };
+};
 
-export const setNotes = ( notes ) => ({
-    type: types.notesLoad,
-    payload: notes
-})
+export const setNotes = (notes) => ({
+  type: types.notesLoad,
+  payload: notes,
+});
 
 export const startSaveNote = (note) => {
-    return async(dispatch, getState) => {
-        const {uid} = getState().auth;
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
 
-        if(!note.url){
-            delete note.url;
-        }
-        const noteToFirestore = {...note};
-        delete noteToFirestore.id;
-        await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
-
+    //el url aveces puede ser nulo, firestore no guarda nulos
+    if (!note.url) {
+      delete note.url;
     }
-}
+    const noteToFirestore = { ...note };
+    delete noteToFirestore.id;
+    await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+
+    dispatch(refreshNote(note.id, noteToFirestore));
+    Swal.fire('Save', note.title,'success')
+  };
+};
+
+export const refreshNote = (id, note) => ({
+    type: types.notesUpdated,
+    payload: {
+        id,
+        note: {
+            id,
+            ...note
+        }
+    }
+})

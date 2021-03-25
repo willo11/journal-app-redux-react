08e1d54b712c52981ff8ebc6c88d5fdf204f02aa
willo11 <47759930+willo11@users.noreply.github.com>
2021-03-25 08:55:11 -0500

    Show Active Note

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,30 +1,54 @@
-import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
+import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
-    return (
-        <div className="notes__main-content">
-            <NotesAppBar />
-            <div className="notes__content">
-                <input 
-                    type="text"
-                    placeholder="Some Awesome Title"
-                    className="notes__title-input"
-                    autoComplete="off"
-                />
-                <textarea
-                    placeholder="What happend Today"
-                    className="notes__textarea"
-                ></textarea>
 
-                <div className="notes__image">
-                    <img 
-                        src="https://static.toiimg.com/photo/72975551.cms"
-                        alt="imagen xD"
-                    />
-                </div>
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange, reset] = useForm(note);
 
-            </div>
-        </div>
-    )
-}
+  const { body, title } = formValues;
+
+  const activeId = useRef(note.id);
+
+  useEffect(() => {
+      if (note.id !== activeId.current) {
+          reset(note);
+          activeId.current = note.id
+      }
+
+
+  }, [note,reset])
+
+  return (
+    <div className="notes__main-content">
+      <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some Awesome Title"
+          className="notes__title-input"
+          autoComplete="off"
+          value={title}
+          onChange={handleInputChange}
+        />
+        <textarea
+          placeholder="What happend Today"
+          className="notes__textarea"
+          value={body}
+          onChange={handleInputChange}
+        ></textarea>
+
+        {note.url && (
+          <div className="notes__image">
+            <img
+              src="https://static.toiimg.com/photo/72975551.cms"
+              alt="imagen xD"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
